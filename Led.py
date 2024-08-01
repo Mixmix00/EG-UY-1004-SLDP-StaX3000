091a89b323fdfe279aa16c194955230822fe26b0
Max Spier <68516760+Mixmix00@users.noreply.github.com>
2024-08-01 14:43:22 -0400

    Make LED code better for changes

--- Led.py
+++ Led.py
@@ -5,19 +5,25 @@ RED_PIN = 12
 GREEN_PIN = 19
 BLUE_PIN = 13
 
-GPIO.setwarnings(False)
+def setup():
+    GPIO.setwarnings(False)
 
-GPIO.setmode(GPIO.BCM)
+    GPIO.setmode(GPIO.BCM)
 
-GPIO.setup(RED_PIN, GPIO.OUT)
-GPIO.setup(GREEN_PIN, GPIO.OUT)
-GPIO.setup(BLUE_PIN, GPIO.OUT)
+    GPIO.setup(RED_PIN, GPIO.OUT)
+    GPIO.setup(GREEN_PIN, GPIO.OUT)
+    GPIO.setup(BLUE_PIN, GPIO.OUT)
 
 # Light Blue
 # Values are inverted (HIGH becomes LOW etc)
-GPIO.output(RED_PIN, GPIO.HIGH)
-GPIO.output(GREEN_PIN, GPIO.LOW)
-GPIO.output(BLUE_PIN, GPIO.LOW)
+# Think of LOW as 0 and HIGH as 255
+def signalStartingRobot():
+    GPIO.output(RED_PIN, GPIO.HIGH)
+    GPIO.output(GREEN_PIN, GPIO.LOW)
+    GPIO.output(BLUE_PIN, GPIO.LOW)
 
-time.sleep(10)
-GPIO.cleanup()
+def signalRobotOnAndNotEnabled():
+    GPIO.output(RED_PIN, GPIO.LOW)
+    GPIO.output(GREEN_PIN, GPIO.HIGH)
+    GPIO.output(BLUE_PIN, GPIO.HIGH)
+
