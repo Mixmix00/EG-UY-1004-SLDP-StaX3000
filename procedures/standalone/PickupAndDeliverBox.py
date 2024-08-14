d14bdc2769c2578f7119e55cd173354b93ff897d
Max Spier <68516760+Mixmix00@users.noreply.github.com>
2024-08-14 00:13:20 -0400

    center

--- procedures/standalone/PickupAndDeliverBox.py
+++ procedures/standalone/PickupAndDeliverBox.py
@@ -3,7 +3,11 @@ import subsystems.AprilTagWebcam as AprilTagWebcam
 import constants.TagID as constants
 import math
 import time
+import LocatePickupBox
+import DriveToPickupBox
 def run():
+    LocatePickupBox.run(1)
+    DriveToPickupBox.run(1)
     initialFrame = AprilTagWebcam.readAndDetectFrame()
     res = initialFrame[1]
 
@@ -24,6 +28,23 @@ def run():
                 frame = AprilTagWebcam.readAndDetectFrame()
                 res = frame[1]
                 center = AprilTagWebcam.isTagInCenterOfCamera2D(frame[0], res, constants.deliveryZoneTop, 0.05)
+            
+            while not center[1]:
+                processed = AprilTagWebcam.findRelative2DPositionOfTagFromCenterOfCamera(frame[0], res, constants.deliveryZoneTop)
+
+                if not processed[1]:
+                    Drive.CONCURRENT_setDriveMotors(math.pi/2)
+                else:
+                    Drive.CONCURRENT_setDriveMotors(-math.pi/2)
+                    
+                time.sleep(0.1)
+
+                frame = AprilTagWebcam.readAndDetectFrame()
+                res = frame[1]
+                center = AprilTagWebcam.isTagInCenterOfCamera2D(frame[0], res, constants.deliveryZoneTop, 0.05)
+            
 
             
+
+
     return
